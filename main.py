1e048c0e83c8873878312d4b58a20b81f3ffa08f
brlga002 <gabriel.lima1000@gmail.com>
2020-08-23 19:39:11 -0400

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+from typing import Optional
+from fastapi import FastAPI, HTTPException
+
+import functions as fc
+
+app = FastAPI(
+    title="Python Rest API",
+    description=
+    "Teste para vaga Desenvolvedor Home Office na Digistarts a versão em note está em <a href=\"https://github.com/brlga002/digistarts\" target=\"_blank\">digistarts</a>",
+    version="1.0.0",
+    openapi_tags=fc.tags_metadata(),
+    docs_url="/",
+    redoc_url="/documentationOnly")
+
+
+@app.post("/soma", tags=["soma"])
+def soma_de_binarios(fistNumber: Optional[str], secondNumber: Optional[str]):
+    dadosValidos = fc.verifica_operacao_permitida(fistNumber, secondNumber)
+    fistNumber = dadosValidos[0]
+    secondNumber = dadosValidos[1]
+    result = int(fistNumber + secondNumber)
+    return fc.formata_zero_esquerda(bin(result))
+
+
+@app.post("/subtracao", tags=["subtracao"])
+def subtracao_de_binarios(fistNumber: Optional[str],
+                          secondNumber: Optional[str]):
+    dadosValidos = fc.verifica_operacao_permitida(fistNumber, secondNumber)
+    fistNumber = dadosValidos[0]
+    secondNumber = dadosValidos[1]
+    fc.verifica_segundo_maior_primeiro(fistNumber, secondNumber)
+    result = int(fistNumber - secondNumber)
+    return fc.formata_zero_esquerda(bin(result))
+
+
+@app.post("/multiplicacao", tags=["multiplicacao"])
+def multiplicacao_dois_numeros_binarios(fistNumber: Optional[str],
+                                        secondNumber: Optional[str]):
+    dadosValidos = fc.verifica_operacao_permitida(fistNumber, secondNumber)
+    fistNumber = dadosValidos[0]
+    secondNumber = dadosValidos[1]
+    result = int(fistNumber * secondNumber)
+    return fc.formata_zero_esquerda(bin(result))
+
+
+@app.post("/divisao", tags=["divisao"])
+def divisao_dois_numeros_binarios(fistNumber: Optional[str],
+                                  secondNumber: Optional[str]):
+    dadosValidos = fc.verifica_operacao_permitida(fistNumber, secondNumber)
+    fistNumber = dadosValidos[0]
+    secondNumber = dadosValidos[1]
+    fc.verifica_segundo_maior_primeiro(fistNumber, secondNumber)
+    result = bin(int(fistNumber / secondNumber))
+
+    if (fistNumber % secondNumber != 0):
+        result = fc.gera_binario_depois_float(fistNumber, secondNumber)
+
+    return fc.formata_zero_esquerda(result)
+
+
+@app.post("/resto", tags=["resto"])
+def resto_da_divisao_dois_numeros_binarios(fistNumber: Optional[str],
+                                           secondNumber: Optional[str]):
+    dadosValidos = fc.verifica_operacao_permitida(fistNumber, secondNumber)
+    fistNumber = dadosValidos[0]
+    secondNumber = dadosValidos[1]
+    fc.verifica_segundo_maior_primeiro(fistNumber, secondNumber)
+    result = int(fistNumber % secondNumber)
+    return fc.formata_zero_esquerda(bin(result))
