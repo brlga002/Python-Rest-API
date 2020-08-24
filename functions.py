f0d377dfca3503ba433c3ca95a9246ceed9e8e9a
brlga002 <gabriel.lima1000@gmail.com>
2020-08-24 10:24:42 -0400

    fix: space in function description_api()

--- functions.py
+++ functions.py
@@ -27,10 +27,8 @@ def tags_metadata():
     return tags_metadata
 
 def description_api():
-    description = ''' 
-        Teste para vaga Desenvolvedor Home Office na Digistarts disponível no repositório: 
-        <a href=\"https://github.com/brlga002/Python-Rest-API\" target=\"_blank\">Python Rest API</a>  
-        Uma versão em note está em <a href=\"https://github.com/brlga002/digistarts\" target=\"_blank\">digistarts</a>'''
+    description = '''Teste para vaga Desenvolvedor Home Office na Digistarts disponível no repositório:
+    <a href=\"https://github.com/brlga002/Python-Rest-API\" target=\"_blank\">Python Rest API</a> Uma versão em note está em <a href=\"https://github.com/brlga002/digistarts\" target=\"_blank\">digistarts</a>'''
     return description
 
 def verifica_operacao_permitida(fistNumber, secondNumber):
